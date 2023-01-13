92a2289caa0b4c0d387596aac554fd3ada53ce0b
salobchyanskiy <salobchyanskiy@Gmail.com>
2023-01-13 19:58:52 +0300

    Update

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from pytube import YouTube
+from colorama import *
 
 
 print("Youtube-vid-downloader v.0.1")
 link = input("Enter a video link: ")
 video = YouTube(link)
-quality = input("Choose a video quality (High/Low)")
+quality = input("Choose a video quality (High/Low): ")
 
-if quality = "High":
-  print("Downloading...")
-  output = video.streams.get_highest_resolution()
-  print("Successfull downloading!")
+if quality == "High":
+    print(f"Downloading video {video.title} from channel {video.author}")
+    output = video.streams.get_highest_resolution()
+    print(Fore.LIGHTGREEN_EX + "Successfully downloading! Check a folder with this program")
   
-if quality = "Low":
-  print("Downloading...")
-  output = video.streams.get_lowest_resolution()
-  print("Successfull downloading!")
-  
-else:
-  print("Incorrect choose!")
+if quality == "Low":
+    print(f"Downloading video {video.title} from channel {video.author}")
+    output = video.streams.get_lowest_resolution()
+    print(Fore.LIGHTGREEN_EX + "Successfully downloading! Check a folder with this program")
 
-  
+
 output.download()
+
+print("Press a random button to exit")
