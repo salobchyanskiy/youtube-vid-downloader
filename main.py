72216fe5f03db2f6ccb97efa2efbaae7314a2d21
salobchyanskiy <salobchyanskiy@Gmail.com>
2023-01-13 18:59:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+
+print("Youtube-vid-downloader v.0.1")
+link = input("Enter a video link: ")
+video = YouTube(link)
+quality = input("Choose a video quality (High/Low)")
+
+if quality = "High":
+  print("Downloading...")
+  output = video.streams.get_highest_resolution()
+  print("Successfull downloading!")
+  
+if quality = "Low":
+  print("Downloading...")
+  output = video.streams.get_lowest_resolution()
+  print("Successfull downloading!")
+  
+else:
+  print("Incorrect choose!")
